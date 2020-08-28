7425610339f4d994a0924b4b99102b22420d8267
Taylor Brazelton <taylor.r.brazelton@gmail.com>
2020-08-28 13:51:50 -0400

    Updated setup.

--- setup.py
+++ setup.py
@@ -1,8 +1,14 @@
 from setuptools import setup
 
+def readme():
+    with open('README.md') as f:
+        return f.read()
+
 setup(name='algoliaqb',
-      version='0.1',
+      version='0.0.1',
       description='Algolia Query Builder',
+      long_description=readme(),
+      keywords='funniest joke comedy flying circus',
       url='http://github.com/ryanb58/algoliaqb',
       author='Taylor Brazelton',
       author_email='taylor.r.brazelton@gmail.com',
